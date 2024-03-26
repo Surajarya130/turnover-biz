17a7316f0ac34eb71d2741057bdf9f7cd2a4338c
surajarya130 <surajarya130@gmail.com>
2024-03-26 14:14:07 +0530

    feat: created a new branch to handle back-end stuff

---
+++ prisma/index.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  // This line declares a global variable that is used to store the prisma client instance
+  var prisma: PrismaClient | undefined;
+}
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+export default prisma;
